4ffd742f3a23306e4af8f87ffa13aff387b9d777
Sam Cork <sam57719@gmail.com>
2026-02-25 21:41:21 +0000

    Add debug prints to display timezone data for various regions

--- src/main.py
+++ src/main.py
@@ -1,4 +1,22 @@
 from utimezone.timezone import TimeZone
 
 timezone = TimeZone('Europe/London')
+print(timezone)
+print(timezone.__dict__)
+print()
+
+timezone = TimeZone('Africa/Bamako')
+print(timezone)
+print(timezone.__dict__)
+print()
+
+
+timezone = TimeZone('America/Miquelon')
+print(timezone)
+print(timezone.__dict__)
+print()
+
+
+timezone = TimeZone('Asia/Oral')
+print(timezone)
 print(timezone.__dict__)
