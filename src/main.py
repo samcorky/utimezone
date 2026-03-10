2d75d0e88c1fbba84f7ce016cc09086d049ba3b2
Sam Cork <sam57719@gmail.com>
2026-03-10 15:44:45 +0000

    Add null checks for DST transition rules across timezones

--- src/main.py
+++ src/main.py
@@ -5,31 +5,69 @@ from utimezone.timezone import TimeZone
 timezone = TimeZone('Europe/London')
 print(timezone)
 print(timezone.__dict__)
-dst_start = timezone._dst_start_rule.get_transition(2023)
-print(dst_start)
-print(time.gmtime(dst_start))
-dst_end = timezone._dst_end_rule.get_transition(2023)
-print(dst_end)
-print(time.gmtime(dst_end))
+if timezone._dst_start_rule is not None:
+    dst_start = timezone._dst_start_rule.get_transition(2023)
+    print(dst_start)
+    print(time.gmtime(dst_start))
+if timezone._dst_end_rule is not None:
+    dst_end = timezone._dst_end_rule.get_transition(2023)
+    print(dst_end)
+    print(time.gmtime(dst_end))
 print()
+
 
 timezone = TimeZone('Africa/Bamako')
 print(timezone)
 print(timezone.__dict__)
+if timezone._dst_start_rule is not None:
+    dst_start = timezone._dst_start_rule.get_transition(2023)
+    print(dst_start)
+    print(time.gmtime(dst_start))
+if timezone._dst_end_rule is not None:
+    dst_end = timezone._dst_end_rule.get_transition(2023)
+    print(dst_end)
+    print(time.gmtime(dst_end))
 print()
 
 
 timezone = TimeZone('America/Miquelon')
 print(timezone)
 print(timezone.__dict__)
+if timezone._dst_start_rule is not None:
+    dst_start = timezone._dst_start_rule.get_transition(2023)
+    print(dst_start)
+    print(time.gmtime(dst_start))
+if timezone._dst_end_rule is not None:
+    dst_end = timezone._dst_end_rule.get_transition(2023)
+    print(dst_end)
+    print(time.gmtime(dst_end))
 print()
+
 
 
 timezone = TimeZone('Asia/Oral')
 print(timezone)
 print(timezone.__dict__)
+if timezone._dst_start_rule is not None:
+    dst_start = timezone._dst_start_rule.get_transition(2023)
+    print(dst_start)
+    print(time.gmtime(dst_start))
+if timezone._dst_end_rule is not None:
+    dst_end = timezone._dst_end_rule.get_transition(2023)
+    print(dst_end)
+    print(time.gmtime(dst_end))
+print()
 
 
 timezone = TimeZone("Pacific/Auckland")
 print(timezone)
 print(timezone.__dict__)
+if timezone._dst_start_rule is not None:
+    dst_start = timezone._dst_start_rule.get_transition(2023)
+    print(dst_start)
+    print(time.gmtime(dst_start))
+if timezone._dst_end_rule is not None:
+    dst_end = timezone._dst_end_rule.get_transition(2023)
+    print(dst_end)
+    print(time.gmtime(dst_end))
+print()
